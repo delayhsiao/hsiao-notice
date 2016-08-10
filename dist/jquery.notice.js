2099d1b564498722786e25196a0af1ec27eead98
delay <fuck@fackyou.org>
2016-08-10 13:40:30 +0800

    init

---
+++ dist/jquery.notice.js
@@ -0,0 +1,57 @@
+$.notice = function(options){
+	var config = {
+		type : null,
+		message : null,
+		callback : function(){},
+		timeout : 1000,
+		context : 'body',
+	};
+
+	init = function(){
+		$('.api-notice').remove();
+		setting = $.extend({},config,options);
+		if(typeof(setting.type) == 'undefined'){
+			console.log('Error:The param "type" is null!');
+			return false;
+		}
+		notice_box = '<div class="api-notice '+setting.type+'" style="display:none"></div>';
+		$(setting.context).after(notice_box);
+		/*if($(document).scrollTop() > $('.api-nav').height()){
+			$('.api-notice').css('top',0);
+		}*/
+		switch(setting.type){
+			case 'success':
+				notice_content = this.success(setting);
+				break;
+			case 'error':
+				notice_content = this.error(setting);
+				break;
+			case 'info':
+				notice_content = this.info(setting);
+				break;
+		}
+		$('.api-notice').html(notice_content).fadeIn();
+		fadeout = setInterval(function(){
+			$('.api-notice').fadeOut();
+			clearInterval(fadeout);
+			setting.callback();
+		},setting.timeout);
+	}
+
+	success = function(setting){
+		content = '<div class="container api-notice-content"><i class="fa fa-check"></i>'+setting.message+'</div>';
+		return content;
+	}
+
+	error = function(setting){
+		content = '<div class="container api-notice-content"><i class="fa fa-exclamation-triangle"></i>'+setting.message+'</div>';
+		return content;
+	}
+
+	info = function(setting){
+		content = '<div class="container api-notice-content"><i class="fa fa-info-circle"></i>'+setting.message+'</div>';
+		return content;
+	}
+
+	init();
+}
