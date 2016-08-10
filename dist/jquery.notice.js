21c928ed4887cd0ff36119d7dbad2372caf256dd
delay <fuck@fackyou.org>
2016-08-10 17:41:55 +0800

    version 0.2.0

--- dist/jquery.notice.js
+++ dist/jquery.notice.js
@@ -1,31 +1,29 @@
 /*!
- * Hsiao-Notice 0.1.0 (http://www.fackyou.org)
+ * Hsiao-Notice 0.2.0 (http://www.fackyou.org)
  * 自用JQuery轻量级提示插件
  * Author：Delay.Hsiao <fuck@fackyou.org/547084615>
  * Licensed under the MIT license
  */
 
-$.notice = function(options){
+$.notice = $.HsiaoNotice = function(options){
 	var config = {
 		type : null,
 		message : null,
 		callback : function(){},
 		timeout : 1000,
 		context : 'body',
+		icon : '',  //加图标，可以是img标签，也可以是fonticon
 	};
 
 	init = function(){
-		$('.api-notice').remove();
+		$('.dn-notice').remove();
 		setting = $.extend({},config,options);
 		if(typeof(setting.type) == 'undefined'){
 			console.log('Error:The param "type" is null!');
 			return false;
 		}
-		notice_box = '<div class="api-notice '+setting.type+'" style="display:none"></div>';
+		notice_box = '<div class="dn-notice '+setting.type+'" style="display:none"></div>';
 		$(setting.context).append(notice_box);
-		/*if($(document).scrollTop() > $('.api-nav').height()){
-			$('.api-notice').css('top',0);
-		}*/
 		switch(setting.type){
 			case 'success':
 				notice_content = this.success(setting);
@@ -37,28 +35,60 @@ $.notice = function(options){
 				notice_content = this.info(setting);
 				break;
 		}
-		$('.api-notice').html(notice_content).fadeIn();
+		$('.dn-notice').html(notice_content).fadeIn();
 		fadeout = setInterval(function(){
-			$('.api-notice').fadeOut();
+			$('.dn-notice').fadeOut();
 			clearInterval(fadeout);
 			setting.callback();
 		},setting.timeout);
 	}
 
 	success = function(setting){
-		content = '<div class="container api-notice-content"><i class="fa fa-check"></i>'+setting.message+'</div>';
+		content = '<div class="container dn-notice-content">'+setting.icon+setting.message+'</div>';
 		return content;
 	}
 
 	error = function(setting){
-		content = '<div class="container api-notice-content"><i class="fa fa-exclamation-triangle"></i>'+setting.message+'</div>';
+		content = '<div class="container dn-notice-content">'+setting.icon+setting.message+'</div>';
 		return content;
 	}
 
 	info = function(setting){
-		content = '<div class="container api-notice-content"><i class="fa fa-info-circle"></i>'+setting.message+'</div>';
+		content = '<div class="container dn-notice-content">'+setting.icon+setting.message+'</div>';
 		return content;
 	}
 
 	init();
+}
+
+var HsiaoNoticeTimeout = 1000;
+
+var HsiaoNotice = {
+	outtime:function(time){
+		HsiaoNoticeTimeout = time;
+	},
+	success:function(message,callback){
+		$.HsiaoNotice({
+			type:'success',
+			message:message,
+			timeout:HsiaoNoticeTimeout,
+			callback:callback
+		});
+	},
+	error:function(message,callback){
+		$.HsiaoNotice({
+			type:'error',
+			message:message,
+			timeout:HsiaoNoticeTimeout,
+			callback:callback
+		});
+	},
+	info:function(message,callback){
+		$.HsiaoNotice({
+			type:'info',
+			message:message,
+			timeout:HsiaoNoticeTimeout,
+			callback:callback
+		});
+	},
 }
